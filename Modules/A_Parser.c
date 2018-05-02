443b16b15c79a23fa30856f30b1757c3854a5ba5
Danila Eremenko <danilaeremenko@mail.ru>
2018-05-02 22:43:06 +0300

    Начинаем писать лексический анализатор

--- Modules/A_Parser.c
+++ Modules/A_Parser.c
@@ -1,49 +1,29 @@
 #include <stdio.h>
+#include "Z_Structs.c"
 
 
-struct token {
-    char type[10];
-    char symbols[50];
-    int length;
+void readNextToken(FILE *file, int currentTokenIndex, char c, struct Token *tokens) {
+    int index = 0;
+    tokens[currentTokenIndex].length = 0;
+    while (c != ' ' && c != '\n' && c != EOF && c != ';') {
+        tokens[currentTokenIndex].symbols[index] = c;
+        tokens[currentTokenIndex].length++;
+        index++;
+        fscanf(file, "%c", &c);
+    }
+
+}
+
+
+void leksParser(char *fileName, struct Token *tokens, int *tokensDigit) {
+    FILE *file = fopen(fileName, "r");
+    char c;
+    *tokensDigit = 0;
+    while (fscanf(file, "%c", &c) != EOF) {
+        readNextToken(file, *tokensDigit, c, tokens);
+        if (c != '\n')//TODO  Œ—“€À‹
+            *tokensDigit = *tokensDigit + 1;
+    }
+
 };
 
-int charParsing(char *fileName) {
-    char c;
-    FILE *file = fopen(fileName, "r");
-    while (fscanf(file, "%c", &c) != EOF)
-        printf("%c", c);
-
-
-    return 0;
-}
-
-int wordParsing(char *fileName) {
-    struct token tokens[50];
-    FILE *file = fopen(fileName, "r");
-    char c;
-    int currentTokenIndex = 0;
-
-    printf("Original Text\n");
-    while (fscanf(file, "%c", &c) != EOF) {
-        if (c == ' ' || c == '\n') {
-            printf("%c", c);
-        } else {
-            int index = 0;
-            while (c != ' ' && c != '\n' && c != EOF && c != ';') {
-                tokens[currentTokenIndex].symbols[index] = c;
-                index++;
-                printf("%c", c);
-                fscanf(file, "%c", &c);
-
-            }
-            currentTokenIndex++;
-            printf("%c", c);
-        }
-
-    }
-    printf("\n\nTokens\n\n");
-    for (int j = 0; j < currentTokenIndex; ++j) {
-        printf("%s\n", tokens[j].symbols);
-    }
-    return 0;
-}
--- main.c
+++ main.c
@@ -1,7 +1,24 @@
+#include "C:\Users\Danila Eremenko\CLionProjects\OberonsCompiler\Modules\A_Parser.c"
 
-#include <stdio.h>
+char adress[] = "C:\\Users\\Danila Eremenko\\CLionProjects\\OberonsCompiler\\Tests\\CompilerTests\\1_Test.txt";
+struct Token tokens[50];
+int tokensDigit;
 
+void printTokens() {
+    for (int i = 0; i < tokensDigit; ++i) {
+        for (int j = 0; j < tokens[i].length; ++j) {
+            printf("%c", tokens[i].symbols[j]);
+        }
+        printf("\n");
+        tokens;
+
+    }
+}
 
 int main() {
+    leksParser(adress, (struct Token *) &tokens, &tokensDigit);
+    printTokens();
+
     return 0;
 }
+
