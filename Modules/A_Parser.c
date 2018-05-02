a418f940f29c8124ee80ec523bba9e040c34d022
Danila Eremenko <danilaeremenko@mail.ru>
2018-05-02 20:15:23 +0300

    Учимся парсить, учимся использовать структуры

--- Modules/A_Parser.c
+++ Modules/A_Parser.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 
+struct token {
+    char type[10];
+    char symbols[50];
+    int length;
+};
+
 int charParsing(char *fileName) {
     char c;
     FILE *file = fopen(fileName, "r");
@@ -11,3 +17,33 @@ int charParsing(char *fileName) {
     return 0;
 }
 
+int wordParsing(char *fileName) {
+    struct token tokens[50];
+    FILE *file = fopen(fileName, "r");
+    char c;
+    int currentTokenIndex = 0;
+
+    printf("Original Text\n");
+    while (fscanf(file, "%c", &c) != EOF) {
+        if (c == ' ' || c == '\n') {
+            printf("%c", c);
+        } else {
+            int index = 0;
+            while (c != ' ' && c != '\n' && c != EOF && c != ';') {
+                tokens[currentTokenIndex].symbols[index] = c;
+                index++;
+                printf("%c", c);
+                fscanf(file, "%c", &c);
+
+            }
+            currentTokenIndex++;
+            printf("%c", c);
+        }
+
+    }
+    printf("\n\nTokens\n\n");
+    for (int j = 0; j < currentTokenIndex; ++j) {
+        printf("%s\n", tokens[j].symbols);
+    }
+    return 0;
+}
