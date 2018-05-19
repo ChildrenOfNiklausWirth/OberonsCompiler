5f6dc5913fa12c0d3094930586614d15c17933cf
Danila Eremenko <danilaeremenko@mail.ru>
2018-05-20 02:28:18 +0300

    Сразу так сделать было конечно же нельзя

--- main.c
+++ main.c
@@ -1,23 +1,12 @@
 #include "C:\Users\Danila Eremenko\CLionProjects\OberonsCompiler\Modules\A_Parser.c"
 
-char adress[] = "C:\\Users\\Danila Eremenko\\CLionProjects\\OberonsCompiler\\Tests\\CompilerTests\\1_Test.txt";
-struct Token tokens[50];
-int tokensDigit;
-
-void printTokens() {
-    for (int i = 0; i < tokensDigit; ++i) {
-        for (int j = 0; j < tokens[i].length; ++j) {
-            printf("%c", tokens[i].symbols[j]);
-        }
-        printf("\n");
-        tokens;
-
-    }
-}
+char adress[] = "C:\\Users\\Danila Eremenko\\CLionProjects\\OberonsCompiler\\Tests\\ProgrammsOnOberon\\1_Test.txt";
+struct TokensFlow tokensFlow;
 
 int main() {
-    leksParser(adress, (struct Token *) &tokens, &tokensDigit);
-    printTokens();
+    tokensFlowInitialize(&tokensFlow);
+    tokensParsing(adress, &tokensFlow);
+    printTokensFlow(tokensFlow);
 
     return 0;
 }
