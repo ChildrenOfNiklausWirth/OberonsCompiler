682c7cc09f4e6dc6c37431ddb22972ba6ef44995
danil <danilaeremenko@mail.ru>
2018-06-21 15:16:39 +0300

    1)struct DeclaredVariables добавлена в main

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-#include "Modules/B_LexAnalyzer.c"
+#include "Modules/B_LexAnalyzer.h"
 
 char adress[] = "C:\\Users\\danil\\CLionProjects\\OberonsCompiler\\Tests\\ProgrammsOnOberon\\1_Test.txt";
 struct TokensFlow tokensFlow;
+struct DeclaredVariables declaredVariables;
 
 int main() {
-    lexAnalysis(adress, &tokensFlow);
-
+    lexAnalysis(adress, &tokensFlow, &declaredVariables);
 
     return 0;
 }
