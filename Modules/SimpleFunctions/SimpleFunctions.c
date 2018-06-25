3ed6d3e1e4c976f618e056a1d9c20aad83f97898
danil <danilaeremenko@mail.ru>
2018-06-25 16:16:41 +0300

    ASH+int_hexToDecimal

--- Modules/SimpleFunctions/SimpleFunctions.c
+++ Modules/SimpleFunctions/SimpleFunctions.c
@@ -8,3 +8,23 @@ int int_contains(int digit, const int mass[], int massSize) {
     return 0;
 
 }
+
+int ASH(int x, int n) {
+    return (int) (x * pow(2, n));
+}
+
+int int_hexToDecimal(int digit) {
+    int result = 0;
+    int multiplier = 1;
+    int currentDigit;
+
+    while (digit != 0) {
+        currentDigit = digit % 10;
+        digit = digit / 10;
+        result += currentDigit * multiplier;
+        multiplier *= 16;
+
+    }
+    return result;
+
+}
