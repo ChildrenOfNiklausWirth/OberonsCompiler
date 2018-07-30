141a2ea06dc2f4d5a6f70e1e1836655eae4247fa
Danila Eremenko <danilaeremenko@mail.ru>
2018-07-31 00:53:14 +0300

    Intermediate commit Fixes are necessary

--- Modules/R_RISC.c
+++ Modules/R_RISC.c
@@ -1,1 +1,162 @@
 #include "R_RISC.h"
+
+void Execute(long start, char inputAddres[], char outputAddress[]) {
+    long opc, a, b, c, nxt;
+    R[14] = 0;
+    R[15] = start + ProgOrg;
+
+    while (true) {
+        nxt = R[15] + 4;
+        IR = M[R[15] / 4];
+        opc = IR / int_hexToDecimal(4000000) % int_hexToDecimal(40);
+        a = IR / int_hexToDecimal(400000) % int_hexToDecimal(10);
+        b = IR / int_hexToDecimal(40000) % int_hexToDecimal(10);
+        c = IR % int_hexToDecimal(40000);
+        if (opc < MOVI)
+            c = R[IR % int_hexToDecimal(10)];
+        else if (opc < BEQ) {
+            c = IR % int_hexToDecimal(40000);
+            if (c >= int_hexToDecimal(20000))
+                c -= int_hexToDecimal(40000);
+        } else {
+            c = IR % int_hexToDecimal(4000000);
+            if (c >= int_hexToDecimal(2000000))
+                c -= int_hexToDecimal(4000000);
+
+
+        }
+        switch (opc) {
+            case MOV:
+                R[a] = ASH(c, b);
+                break;
+            case MOVI:
+                R[a] = ASH(c, b);
+                break;
+            case MVN:
+                R[a] = -ASH(c, b);
+                break;
+            case MVNI:
+                R[a] = -ASH(c, b);
+            case ADD:
+                R[a] = R[b] + c;
+                break;
+            case ADDI:
+                R[a] = R[b] + c;
+                break;
+            case SUB:
+                R[a] = R[b] - c;
+                break;
+            case SUBI:
+                R[a] = R[b] - c;
+                break;
+            case MUL:
+                R[a] = R[b] * c;
+                break;
+            case MULI:
+                R[a] = R[b] * c;
+                break;
+            case Div:
+                R[a] = R[b] / c;
+                break;
+            case DIVI:
+                R[a] = R[b] / c;
+                break;
+            case Mod:
+                R[a] = R[b] % c;
+                break;
+            case MODI:
+                R[a] = R[b] % c;
+                break;
+            case CMP:
+                Z = (R[b] == c);
+                N = R[b] < c;
+                break;
+            case CMPI:
+                Z = (R[b] == c);
+                N = R[b] < c;
+                break;
+            case CHKI:
+                if (R[a] < 0 || R[a] >= c)
+                    R[a] = 0;
+                break;
+            case LDW:
+                R[a] = M[(R[b] + c) / 4];
+                break;
+            case LDB:
+                //Не реализуется
+                break;
+            case POP:
+                R[a] = M[R[b] / 4];
+                R[b] += c;
+                break;
+            case STW:
+                M[(R[b] + c) / 4] = R[a];
+                break;
+            case STB:
+                //не реализуется
+                break;
+            case PSH:
+                R[b] -= c;
+                M[R[b] / 4] = R[a];
+                break;
+            case RD:
+                //TODO
+                break;
+            case WRD:
+                //TODO
+                break;
+            case WRH:
+                //TODO
+                break;
+            case WRL:
+                //TODO
+                break;
+            case BEQ:
+                if (Z)
+                    nxt = R[15] + c * 4;
+                break;
+            case BNE:
+                if (!Z)
+                    nxt = R[15] + c * 4;
+                break;
+            case BLT:
+                if (N)
+                    nxt = R[15] + c * 4;
+                break;
+            case BGE:
+                if (!N)
+                    nxt = R[15] + c * 4;
+                break;
+            case BLE:
+                if (Z || N)
+                    nxt = R[15] + c * 4;
+                break;
+            case BGT:
+                if (!Z & !N)
+                    nxt = R[15] + c * 4;
+                break;
+            case BR:
+                nxt = R[15] + c * 4;
+                break;
+            case BSR:
+                nxt = R[15] + c * 4;
+                R[14] = R[15] + 4;
+                break;
+            case RET:
+                nxt = R[c % int_hexToDecimal(10)];
+                if (nxt == 0)
+                    break;
+        }
+        R[15] = nxt;
+
+
+    }
+}
+
+void Load(long code[], long len) {
+    int i = 0;
+    while (i < len) {
+        M[i + ProgOrg / 4] = code[i];
+        i++;
+    };
+}
--- Modules/R_RISC.h
+++ Modules/R_RISC.h
@@ -1,4 +1,56 @@
 #ifndef OBERONSCOMPILER_C_R_RISC_H
 #define OBERONSCOMPILER_C_R_RISC_H
 
+#include <stdbool.h>
+#include <stdio.h>
+#include "SimpleFunctions/SimpleFunctions.h"
+
+#define MemSize 4096
+#define ProgOrg 2048
+#define MOV 0
+#define MVN 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define Div 5
+#define Mod 6
+#define CMP 7
+#define MOVI 16
+#define MVNI 17
+#define ADDI 18
+#define SUBI 19
+#define MULI 20
+#define DIVI 21
+#define MODI 22
+#define CMPI 23
+#define CHKI 24
+#define LDW 32
+#define LDB 33
+#define POP 34
+#define STW 36
+#define STB 37
+#define PSH 38
+#define RD 40
+#define WRD 41
+#define WRH 42
+#define WRL 43
+#define BEQ 48
+#define BNE 49
+#define BLT 50
+#define BGE 51
+#define BLE 52
+#define BGT 53
+#define BR 56
+#define BSR 57
+#define RET 58
+
+
+long IR;
+bool N, Z;
+long R[16];
+long M[MemSize / 4];
+FILE W;
+
+void Execute(long start, char inputAddres[], char outputAddress[]);
+
 #endif //OBERONSCOMPILER_C_R_RISC_H
